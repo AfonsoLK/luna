46cad13a837a4f1d3495392ddecb281deb175c69
Lucas <afonsos.lucassoares@gmail.com>
2025-07-08 23:24:09 -0300

    Refactor URL configuration and enhance frontend with Tanstack Query integration. Added API routing in urls.py, and updated package.json and pnpm-lock.yaml to include @tanstack/react-query and related packages. Integrated TanstackProvider and ReactQueryDevtools in layout.tsx for improved data fetching capabilities.

---
+++ backend/core/api.py
@@ -0,0 +1,6 @@
+# from ninja import NinjaAPI
+# from app.api import optimizer_ router
+# from scalar_django_ninja import ScalarViewer
+
+# api = NinjaAPI (docs=ScalarViewer (), docs_url='/docs/')
+# api. add_router('/', optimizer_router)
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { TanstackProvider } from "@/components/providers/tanstack-provider";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <TanstackProvider>
+          {children}
+          <ReactQueryDevtools initialIsOpen={false} />
+        </TanstackProvider>
       </body>
     </html>
   );
